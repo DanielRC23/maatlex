2ada62a8115d25a63e0d02da28b4c0440e750268
Daniel <oswi_06@outlook.com>
2026-06-07 14:32:37 -0600

    Primer avance MAAT LEX

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const menuBtn = document.getElementById('menu-btn');
+
+const mobileMenu =
+document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+
+mobileMenu.classList.toggle('active');
+
+});
+
+const faqQuestions =
+document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+
+question.addEventListener('click', () => {
+
+const answer =
+question.nextElementSibling;
+
+if(answer.style.maxHeight){
+
+answer.style.maxHeight = null;
+
+}else{
+
+answer.style.maxHeight =
+answer.scrollHeight + "px";
+
+}
+
+});
+
+});
+
+if ('serviceWorker' in navigator) {
+
+window.addEventListener('load', () => {
+
+navigator.serviceWorker.register('/service-worker.js')
+
+.then(() => {
+
+console.log('Service Worker registrado');
+
+})
+
+.catch(err => {
+
+console.log(err);
+
+});
+
+});
+
+}
+
+let deferredPrompt;
+
+const installBtn =
+document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+
+e.preventDefault();
+
+deferredPrompt = e;
+
+installBtn.style.display = 'inline-block';
+
+});
+
+installBtn.addEventListener('click', async () => {
+
+if(!deferredPrompt) return;
+
+deferredPrompt.prompt();
+
+const choice =
+await deferredPrompt.userChoice;
+
+deferredPrompt = null;
+
+});
