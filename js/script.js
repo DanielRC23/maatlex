448a818451221751388105405d8e5651932713f4
Daniel <oswi_06@outlook.com>
2026-06-07 23:13:06 -0600

    Quitar service worker temporal v1

--- js/script.js
+++ js/script.js
@@ -34,27 +34,6 @@ answer.scrollHeight + "px";
 
 });
 
-if ('serviceWorker' in navigator) {
-
-window.addEventListener('load', () => {
-
-navigator.serviceWorker.register('/service-worker.js')
-
-.then(() => {
-
-console.log('Service Worker registrado');
-
-})
-
-.catch(err => {
-
-console.log(err);
-
-});
-
-});
-
-}
 
 let deferredPrompt;
 
